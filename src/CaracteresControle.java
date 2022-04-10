19722226517b1e8054a5a04049294a7b487ec4cf
Gustavo Simões <gustavo.joi@hotmail.com>
2022-04-10 11:50:11 -0300

    ajuste nos caracteres de controle

--- src/CaracteresControle.java
+++ src/CaracteresControle.java
@@ -1,11 +1,18 @@
 
 public class CaracteresControle {
-	
-	public static final char ENQ = '\u0005';
-	public static final char ACK = '\u0006';
-	public static final char EOT = '\u0004';
-	public static final char STX = '\u0002';
-	public static final char CR = '\u0013';
-	public static final char LF = '\u0010';
-	public static final char ETX = '\u0003';
+	public static final char STX = '\002';
+	public static final char ETX = '\003';
+	public static final char ETB = '\027';
+	public static final char EOT = '\004';
+	public static final char ENQ = '\005';
+	public static final char ACK = '\006';
+	public static final char NAK = '\025';
+	public static final char CR = '\r';
+	public static final char LF = '\n';
+	public static final char MOR = '>';
+	public static final char FS = '\034';
+	public static final char GS = '\035';
+	public static final char RS = '\036';
+	public static final char SFS = '\027';
+	public static final char VT = 0x0B; //END OF BLOCK 011
 }
